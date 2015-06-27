b7c8a8758801d4f789456db6132c592c52e4b413
willowcheng <e.wingus@Gmail.com>
2015-06-26 21:48:29 -0400

    Add login manager

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, g
+from flask.ext.login import LoginManager
 import models
 
 app = Flask(__name__)
+app.secret_key = 'fsdfl;jsdkl;v/.xsdlkjlvkxcvleifjsdkjlj234wer90fs'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 # DEBUG = True
 # PORT = 8000
@@ -13,11 +26,13 @@ def before_request():
     g.db = models.DATABASE
     g.db.connect()
 
+
 @app.after_request
 def after_request(response):
     """"Close the database connection after each request."""
     g.db.close()
     return response
+
 
 @app.route('/')
 def hello_world():
@@ -25,4 +40,11 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    models.initialize()
+    models.User.create_user(
+        name='willowcheng',
+        email='e.wingus@gmail.com',
+        password='password',
+        admin=True
+    )
     app.run()
