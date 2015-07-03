766792345bca56c688b10844ab6860d1ac06020f
willowcheng <e.wingus@Gmail.com>
2015-07-03 12:46:43 -0400

    Registration view

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, g
+from flask import Flask, g, render_template, flash, redirect, url_for
 from flask.ext.login import LoginManager
+
+import forms
 import models
 
 app = Flask(__name__)
@@ -16,10 +18,6 @@ def load_user(userid):
     except models.DoesNotExist:
         return None
 
-# DEBUG = True
-# PORT = 8000
-# HOST = '0.0.0.0'
-
 @app.before_request
 def before_request():
     """Connect to the database before each request."""
@@ -33,11 +31,23 @@ def after_request(response):
     g.db.close()
     return response
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Yay, you registered!", "success")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    return 'Hey'
 
 if __name__ == '__main__':
     models.initialize()
